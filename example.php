9fdc18135bd785a2290f46520f5bf9f3f7edb517
nate <nate@nate.gg>
2020-08-31 04:53:03 -0400

    Example file.

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+require('discordOAuth.php');
+
+// Find your information here: https://discord.com/developers/applications
+$discord = new discordOauth("Application Client ID", "Application Client Secret", "OAuth2 scope", "Redirect URI");
+
+// After using login function Discord will return a code to the Redirect URI.
+if(isset($_GET['code'])) {
+    $discord->getAccessToken($_GET['code']);
+}
+
+if(!$discord->loggedIn()) {
+    $discord->login();
+} else {
+    $user = $discord->getUser();
+    var_dump($user);
+}
+?>
